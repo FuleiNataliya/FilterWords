4552e07625a5586433970d08e32c99e0fee2af28
Fulei Nataliya <fulei.nataliya@gmail.com>
2019-07-15 23:18:22 +0300

    added methods

---
+++ src/main/java/FilterWords.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+
+public class FilterWords {
+
+    private FileReader fr;
+    private int count;
+    private int count1;
+    private Map<String, Integer> songWordMap;
+    private List<String> ignored;
+
+    public FilterWords(String fileName) throws FileNotFoundException {
+
+        fr = new FileReader(fileName);
+        songWordMap = new TreeMap<>();
+        ignored = new ArrayList<>();
+
+    }
+
+    public int getCount1() {
+        return count1;
+    }
+
+    public List<String> getIgnored() {
+        return ignored;
+    }
+
+    public int calculateCount() throws IOException {
+        BufferedReader br = new BufferedReader(fr);
+        String line = br.readLine();
+        count = 0;
+        while (line != null) {
+            String[] parts = line.split("[\\p{Punct}\\p{Space}]+");
+            for (String word : parts) {
+                count++;
+                if (word.length() < 3) {
+                    ignored.add(word);
+                    count1++;
+                } else if (songWordMap.containsKey(word)) {
+                    int count = songWordMap.get(word) + 1;
+                    songWordMap.replace(word, count);
+                } else {
+                    songWordMap.put(word, 1);
+                }
+            }
+
+            line = br.readLine();
+        }
+        return count;
+    }
+
+    public void findWords() {
+        List<String> commonWord = new ArrayList<>();
+        for (Map.Entry<String, Integer> pair : songWordMap.entrySet()) {
+            if (pair.getValue() > 3) {
+                commonWord.add(pair.getKey());
+            }
+        }
+        System.out.println("Most often found: " + commonWord);
+    }
+}
+
+
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        FilterWords filterWords = new FilterWords("src/main/resources/data.txt");
+        System.out.println("The total number of words in the text: " + filterWords.calculateCount());
+        System.out.println("The number of words is less than three characters: " + filterWords.getCount1());
+        System.out.println(filterWords.getIgnored());
+        filterWords.findWords();
     }
 }
